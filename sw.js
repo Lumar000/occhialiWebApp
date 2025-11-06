3cb0de89b447369b26f57a2674f6235c0bf6914e
Lumar000 <166657188+Lumar000@users.noreply.github.com>
2025-11-06 09:22:11 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker minimale - serve solo a rendere l'app installabile.
+self.addEventListener('fetch', function(event) {
+  // Non fa nulla, lascia che la richiesta vada alla rete.
+});
